49716fb77ef846d9df4ba1d9cfda3d76e3ffa745
BenPare21 <fireiceben@gmail.com>
2025-04-06 22:15:28 -0400

    Module 5 worked on

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const express = require("express");
-const path = require("path");
+// did get rid of some stuff very similiar btw
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan')
 const hbs = require("hbs");
+
+//Define routers 
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/index');
+var travelRouter = require('./app_server/routes/index');
+var apiRouter = require('./app_server/routes/index');
+
+var handlebars = require('hbs');
 
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 
-const app = express();
+
+//Bring in the database
+require('./app_api/models/db')
+
+var app = express()
+
+app.set('views', path.join(_dirname,'app_server','views'));
+
+handlebars.registerPartials(_dirname+ 'app_server/views/partials')
 
 // Set view engine to Handlebars
 app.set("view engine", "hbs");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser());
+app.use(express.static(path.join(_dirname, 'public')))
+
+//wire up routes to controllers
+app.use('/', indexRouter);
+app.use('users', usersRouter);
+app.use('/travel',travelRouter);
+app.use('.api',apiRouter);
+
+
 app.set("views", path.join(__dirname, "views"));
 
 // Register partials
